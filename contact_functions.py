f826c7896107fd3771b0ae72c02a66aa6c4300cc
e-mehegan <14220@coderacademy.edu.au>
2023-04-26 12:49:37 +1000

    function file added

---
+++ contact_functions.py
@@ -0,0 +1,18 @@
+# add imports
+
+#defs for each functions of the application
+
+def view_contact():
+    print("View Contact Book")
+
+
+def add_contact():
+    print("Add New Contact")
+
+
+def delete_contact():
+    print("Delete a Contact")
+
+
+def edit_contact():
+    ("Edit a Contact")
