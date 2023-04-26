0b4246d1e3a646c1ca3b720af63ef55e729ba526
e-mehegan <14220@coderacademy.edu.au>
2023-04-26 12:25:18 +1000

    Contact menu created

---
+++ main.py
@@ -0,0 +1,16 @@
+#import packages here
+
+#import other pages here
+
+#add different colors here
+
+print("CONTACT BOOK")
+
+def contact_menu():
+    print("Enter 1 to view Contact Book")
+    print("Enter 2 to add new contact")
+    print("Enter 3 to delete a contact")
+    print("Enter 4 to edit a contact")
+    print("Enter 5 to exit the Contact Book")
+    choice = input("Enter your number choice!")
+    return choice
