f07bc7f022ca49a7a97ff2010da8327b354785cb
e-mehegan <14220@coderacademy.edu.au>
2023-04-27 16:57:12 +1000

    add contacts function update

--- contact_functions.py
+++ contact_functions.py
@@ -1,13 +1,19 @@
-# add imports
+# Imports for this file
+import csv
 
 #defs for each functions of the application
-
 def view_contact(file_name):
     print("View Contact Book")
 
 
 def add_contact(file_name):
-    print("Add New Contact")
+    print("ADD NEW CONTACT")
+    name = input("Enter Name: ")
+    address = input("Enter Address: ")
+    phone = input("Enter Phone Number: ")
+    with open(file_name, "a") as contact_file:
+        writer = csv.writer(contact_file)
+        writer.writerow([name, address, phone])
 
 
 def delete_contact(file_name):
