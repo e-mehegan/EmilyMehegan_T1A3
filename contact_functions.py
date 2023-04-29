ca672737c399bf606326edccec5eb5198fac9ae1
e-mehegan <14220@coderacademy.edu.au>
2023-04-29 16:30:11 +1000

    docstrings and PEP8 update

--- contact_functions.py
+++ contact_functions.py
@@ -1,43 +1,56 @@
 # Imports for this file
 import csv
 
-#defs for each functions of the application
-# Function to view the contact book as a whole 
+
+
+#Function to view contacts
 def view_contact(file_name):
+    """Prints all the contacts that are present in the contact file"""
     print("VIEW CONTACT BOOK")
+    """Opens the contact file in read mode, printing the contact file content"""
     with open(file_name, "r") as contact_file:
         reader = csv.reader(contact_file)
         for row in reader:
             print(row)
 
-# Function to add new contacts to the contact book
+
+#Function to add new contacts to the contact book
 def add_contact(file_name):
+    """Takes input from the user to create the new contact"""
     print("ADD NEW CONTACT")
     name = input("Enter Name: ")
     address = input("Enter Address: ")
     phone = input("Enter Phone Number: ")
+    """Opens the contact file in append mode to allow new data to be written into the file.
+    This adds to exisiting data and does not overwrite content in the contact file. Print it in a row
+    """
     with open(file_name, "a") as contact_file:
         writer = csv.writer(contact_file)
         writer.writerow([name, address, phone])
 
-# Function to delete a contact
+
+#Function to delete a contact
 def delete_contact(file_name):
+    """Takes input from the user of which contact they want to delete"""
     print("DELETE A CONTACT")
     contact_remove = input("Enter the contact name that you want to remove: ")
     contact_names = []
-    # Reading and saving the data in a list except the one that we want to remove
+    """Reading and saving the data in a list except the one that we want to remove"""
     with open(file_name, "r") as contact_file:
         reader = csv.reader(contact_file)
-    # Ensuring no case sensitive issues through using .lower
+        """Ensuring no case sensitive issues through using lower. and appends the contact file"""
         for row in reader:
             if(contact_remove.lower() != row[0].lower()):
                 contact_names.append(row)
     print(contact_names)
-    # Writing the updated contact information to the file
+    """Writing the updated contact information to the file"""
     with open(file_name, "w") as contact_file:
         writer = csv.writer(contact_file)
         writer.writerows(contact_names)
 
 
+
+#Function to edit a contact
 def edit_contact(file_name):
-    ("Edit a Contact")
+    """Takes input of which contact user wants to edit"""
+    print("EDIT A CONTACT")
