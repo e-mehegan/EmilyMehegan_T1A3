f39f0fea57870a2ea5c9b7255645f6724d46c062
e-mehegan <14220@coderacademy.edu.au>
2023-04-29 12:09:59 +1000

    delete function added

--- contact_functions.py
+++ contact_functions.py
@@ -2,6 +2,7 @@
 import csv
 
 #defs for each functions of the application
+# Function to view the contact book as a whole 
 def view_contact(file_name):
     print("VIEW CONTACT BOOK")
     with open(file_name, "r") as contact_file:
@@ -9,7 +10,7 @@ def view_contact(file_name):
         for row in reader:
             print(row)
 
-
+# Function to add new contacts to the contact book
 def add_contact(file_name):
     print("ADD NEW CONTACT")
     name = input("Enter Name: ")
@@ -19,9 +20,23 @@ def add_contact(file_name):
         writer = csv.writer(contact_file)
         writer.writerow([name, address, phone])
 
-
+# Function to delete a contact
 def delete_contact(file_name):
-    print("Delete a Contact")
+    print("DELETE A CONTACT")
+    contact_remove = input("Enter the contact name that you want to remove: ")
+    contact_names = []
+    # Reading and saving the data in a list except the one that we want to remove
+    with open(file_name, "r") as contact_file:
+        reader = csv.reader(contact_file)
+    # Ensuring no case sensitive issues through using .lower
+        for row in reader:
+            if(contact_remove.lower() != row[0].lower()):
+                contact_names.append(row)
+    print(contact_names)
+    # Writing the updated contact information to the file
+    with open(file_name, "w") as contact_file:
+        writer = csv.writer(contact_file)
+        writer.writerows(contact_names)
 
 
 def edit_contact(file_name):
