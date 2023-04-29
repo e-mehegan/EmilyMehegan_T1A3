21027aa595dde1f7bb77551d10ceba390474e301
e-mehegan <14220@coderacademy.edu.au>
2023-04-29 17:02:53 +1000

    main.py pep8 style edit

--- main.py
+++ main.py
@@ -9,37 +9,39 @@ print("CONTACT BOOK")
 # The contact list which will display the input from the user
 file_name = "contact_list.csv"
 
-# Check if contact list is created - if not this will create the list
+"""Check if contact list is created - if not this will create the list"""
 try:
     contact_file = open(file_name, "r")
     contact_file.close()
 
-# If does not exist this will create it
+    """If the file does not exist this will create it"""
 except FileNotFoundError as e:
     contact_file = open(file_name, "w")
     contact_file.write("Contacts,Address,Phone\n")
     contact_file.close()
 
-# Contact menu for the application
+
 def contact_menu():
+    # Contact menu for the application
     print("Enter 1 to view Contact Book")
     print("Enter 2 to add new contact")
     print("Enter 3 to delete a contact")
     print("Enter 4 to edit a contact")
     print("Enter 5 to exit the Contact Book")
+    """Asks for user choice and will go to user choice and carry out that function"""
     choice = input("Enter your number choice!: ")
     return choice
 
 
-# Choices for the user and what the input will do - This will loop until user exits
 user_choice = ""
+"""Choices for the user and what the input will do - This will loop until user exits"""
 
-# This is to exit the loop
+
 while user_choice != "5":
     user_choice = contact_menu()
+    """This is to exit the loop"""
 
-#Add to brackets
-
+    """User choice input will go to function number the user choses"""
     if (user_choice == "1"):
         view_contact(file_name)
     elif (user_choice == "2"):
@@ -53,7 +55,7 @@ while user_choice != "5":
     else:
         print("Sorry! Invaild input")
 
-# Brings the user back to the menu
+# Prompt to bring the user back to the contact menu
     input("To continue press Enter...")
 
 # Text for closing application
